e06ad2cee8afc76ed91345691da044d21e78dcec
Clara Helena <106986725+clarasenx@users.noreply.github.com>
2025-10-09 16:12:32 -0400

    adjusts

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,8 +1,36 @@
+import { FilterProductType } from '../dtos/product/filter-product.dto';
+import { ListProductDTO } from '../dtos/product/list-product.dto';
 import Product from "../models/product.model";
 import { BaseRepository } from './base.repository';
 
 export class ProductRepository extends BaseRepository<Product> {
   constructor() {
     super(Product)
+  }
+
+  async listById(id: number): Promise<Product | null> {
+    const product = await Product.findByPk(
+      id,
+      {
+        include: [ { association: 'category' } ]
+      }
+    )
+    return product ? new ListProductDTO(product).getAll() as Product : null
+  }
+
+  async listAll(filters?: FilterProductType): Promise<Product[]> {
+
+    const products = await Product.findAll({
+      where: {
+        ...filters
+      },
+      include: [ { association: 'category' } ]
+    })
+
+    const aux = products.map(product => {
+      return new ListProductDTO(product).getAll()
+    })
+
+    return aux as Product[]
   }
 }
